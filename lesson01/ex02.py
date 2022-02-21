0956990f205eabbb3f52beef012a6e37264e5714
MVaschilova <94765299+MVaschilova@users.noreply.github.com>
2022-02-22 02:36:34 +0300

    Update ex02.py

--- lesson01/ex02.py
+++ lesson01/ex02.py
@@ -6,10 +6,5 @@ time_in_sec = int(input("Введите время в секундах >>> "))
 hours = time_in_sec // 3600
 minutes = time_in_sec // 60 - hours * 60
 seconds = time_in_sec % 60
-print(time_in_sec, 'секунд - это: {0} часов, {1} минут {2} секунд'.format(hours, minutes, seconds))
-
-# вариант 2: более компактный способ
-time_in_sec_short = int(input("Введите время в секундах >>> "))
-print(time_in_sec_short, 'секунд - это {:.0f} часов {:.0f} минут  {:.0f} секунд'.format(time_in_sec // 3600,
-                                                                                        time_in_sec // 60 - hours * 60,
-                                                                                        time_in_sec % 60))
+#print(time_in_sec, 'секунд - это: {0} часов, {1} минут {2} секунд'.format(hours, minutes, seconds))
+print(f'В формате чч:мм:сс {hours:02d}:{minutes:02d}:{seconds:02d}')
